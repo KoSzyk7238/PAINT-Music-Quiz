403e2f3cc0baecd800452e83b19dc1434b534d02
Ignacy Kopka <ignacy.kopka.stud@pw.edu.pl>
2026-05-22 16:29:47 +0200

    Fix syntax error in shuffleArray in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -330,14 +330,14 @@ function GameView() {
     }
   }, [selectedRandomGenreId, selectedQuizForPreview, genres]);
 
-  const shuffleArray = (array) => { {
+  const shuffleArray = (array) => {
     const newArr = [...array];
     for (let i = newArr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
     }
     return newArr;
-  }
+  };
 
   const handleSelectQuizForPreview = async (quiz) => {
     setSelectedQuizForPreview(quiz);
